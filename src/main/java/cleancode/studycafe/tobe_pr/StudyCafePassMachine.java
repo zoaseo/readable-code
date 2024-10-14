ec83008e8766b514ba112ded229e349a2bf19ce5
zoaseo <huijaeseo18@google.com>
2024-10-14 21:16:04 +0900

    리팩토링 (1) - 추상화 레벨

---
+++ src/main/java/cleancode/studycafe/tobe_pr/StudyCafePassMachine.java
@@ -0,0 +1,87 @@
+package cleancode.studycafe.tobe_pr;
+
+import cleancode.studycafe.tobe_pr.io.InputHandler;
+import cleancode.studycafe.tobe_pr.io.OutputHandler;
+import cleancode.studycafe.tobe_pr.io.StudyCafeFileHandler;
+import cleancode.studycafe.tobe_pr.model.StudyCafeLockerPass;
+import cleancode.studycafe.tobe_pr.model.StudyCafePass;
+import cleancode.studycafe.tobe_pr.model.StudyCafePassType;
+import cleancode.studycafe.tobe_pr.exception.AppException;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StudyCafePassMachine {
+
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+    private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+
+    public void run() {
+        try {
+            outputHandler.showWelcomeMessage();
+            outputHandler.showAnnouncement();
+
+            StudyCafePass selectedPass = getSelectedPass();
+
+            Optional<StudyCafeLockerPass> optionalLockerPass = selectLockerPass(selectedPass);
+
+            optionalLockerPass.ifPresentOrElse(
+                    lockerPass -> outputHandler.showPassOrderSummary(selectedPass, lockerPass),
+                    () -> outputHandler.showPassOrderSummary(selectedPass)
+            );
+        } catch (AppException e) {
+            outputHandler.showSimpleMessage(e.getMessage());
+        } catch (Exception e) {
+            outputHandler.showSimpleMessage("알 수 없는 오류가 발생했습니다.");
+        }
+    }
+
+    private StudyCafePass getSelectedPass() {
+        outputHandler.askPassTypeSelection();
+        StudyCafePassType passType = inputHandler.getPassTypeSelectingUserAction();
+
+        List<StudyCafePass> passCandidates = findPassCandidatesBy(passType);
+
+        outputHandler.showPassListForSelection(passCandidates);
+        return inputHandler.getSelectPass(passCandidates);
+    }
+
+    private List<StudyCafePass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
+        List<StudyCafePass> allPasses = studyCafeFileHandler.readStudyCafePasses();
+
+        return allPasses.stream()
+                .filter(studyCafePass -> studyCafePass.isSamePassType(StudyCafePassType.HOURLY))
+                .toList();
+    }
+
+    private Optional<StudyCafeLockerPass> selectLockerPass(StudyCafePass selectedPass) {
+        // 고정 좌석 타입이 아닌가?
+        // 사물함 옵션을 사용할 수 있는 타입이 아닌가?
+        if (selectedPass.cannotUseLocker()) {
+            return Optional.empty();
+        }
+
+        StudyCafeLockerPass lockerPassCandidate = findLockerPassCandidateBy(selectedPass);
+
+        if (lockerPassCandidate != null) {
+            outputHandler.askLockerPass(lockerPassCandidate);
+            boolean isLockerSelected = inputHandler.getLockerSelection();
+
+            if (isLockerSelected) {
+                return Optional.of(lockerPassCandidate);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private StudyCafeLockerPass findLockerPassCandidateBy(StudyCafePass pass) {
+        List<StudyCafeLockerPass> allLockerPasses = studyCafeFileHandler.readLockerPasses();
+
+        return allLockerPasses.stream()
+                .filter(pass::isSameDurationType)
+                .findFirst()
+                .orElse(null);
+    }
+}
---
+++ src/main/java/cleancode/studycafe/tobe_pr/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.tobe_pr.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
